b8be5c3969107915cc0568721e8aa16f0d01c338
Inoue Daiki <daikinissan@gmail.com>
2020-10-22 02:16:12 +0900

    fix: style

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,17 @@
 <template>
-  <b-row>
+  <b-row align-h="center">
     <b-col cols="12" sm="6">
       <b-card no-body>
-        <b-card-body>
-          <b-card-text>ユーザ名</b-card-text>
+        <b-card-body class="py-2">
+          <b-card-text>
+            <strong>ユーザ名</strong>
+          </b-card-text>
         </b-card-body>
 
-        <b-card-img src="https://picsum.photos/200"></b-card-img>
+        <b-card-img
+          src="https://picsum.photos/200"
+          class="rounded-0"
+        ></b-card-img>
 
         <b-card-body>
           <b-card-text>
@@ -25,7 +30,10 @@
                 class="p-0 border-0 shadow-none"
               ></b-form-input>
               <b-input-group-append>
-                <b-button variant="link text-decoration-none">
+                <b-button
+                  variant="link"
+                  class="text-decoration-none shadow-none"
+                >
                   投稿する
                 </b-button>
               </b-input-group-append>
