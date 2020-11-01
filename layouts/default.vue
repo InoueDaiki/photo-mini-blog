9a889c718e4921e4fd6556169460f118b66b5066
Inoue Daiki <daiki.inoue@automagi.jp>
2020-11-01 17:02:38 +0900

    Revert "docs: init default layout"
    This reverts commit 4b1d32c1ec4a5107dbc10804c44b508f7c87a2be.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,16 @@
 <template>
-  <div></div>
+  <div>
+    <b-navbar toggleable="sm" variant="light" sticky class="border-bottom">
+      <b-container>
+        <nuxt-link to="/">
+          <b-navbar-brand>Photo mini blog</b-navbar-brand>
+        </nuxt-link>
+      </b-container>
+    </b-navbar>
+    <b-container class="my-3">
+      <Nuxt />
+    </b-container>
+  </div>
 </template>
 
 <script>
