45afb3854e9efbbed0e3d96d8b59c972a0837b48
Inoue Daiki <daiki.inoue@automagi.jp>
2020-11-01 15:07:41 +0900

    docs: disable S3 in index page

--- pages/index.vue
+++ pages/index.vue
@@ -14,7 +14,7 @@
 
 <script>
 import { mapState } from 'vuex'
-import { API, graphqlOperation, Storage } from 'aws-amplify'
+import { API, graphqlOperation } from 'aws-amplify'
 import { listPosts } from '@/assets/graphql/queries'
 import { createComment } from '~/assets/graphql/mutations'
 
@@ -59,7 +59,9 @@ export default {
               comment.createdAt = new Date(comment.createdAt).toLocaleString()
               return comment
             })
-            post.imageUrl = await Storage.get(post.s3key)
+            // TODO: use S3
+            await new Promise((resolve) => setTimeout(resolve, 1000))
+            post.imageUrl = 'https://picsum.photos/400/300'
             return post
           })
       )
