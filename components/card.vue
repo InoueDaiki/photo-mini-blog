9d279ad17f263ca3f397c52d4ad07a4b151189e2
Inoue Daiki <daiki.inoue@automagi.jp>
2020-11-01 17:02:40 +0900

    Revert "docs: remove comment form"
    This reverts commit d072a6f76b26a66712fe76912ef8d9f92aaeae41.

--- components/card.vue
+++ components/card.vue
@@ -1,10 +1,31 @@
 <template>
-  <b-card :img-src="post.imageUrl">
+  <b-card :img-src="post.imageUrl" footer-bg-variant="white">
     <div v-for="comment in post.comments.items" :key="comment.id">
       <strong>{{ comment.username }}</strong>
       {{ comment.content }} <br />
       <small>{{ comment.createdAt }}</small>
     </div>
+
+    <template #footer>
+      <b-form>
+        <b-input-group>
+          <b-form-input
+            v-model="newComment"
+            placeholder="コメントを追加..."
+            class="border-0 shadow-none"
+          ></b-form-input>
+          <b-input-group-append>
+            <b-button
+              type="submit"
+              variant="link"
+              class="text-decoration-none shadow-none"
+            >
+              投稿する
+            </b-button>
+          </b-input-group-append>
+        </b-input-group>
+      </b-form>
+    </template>
   </b-card>
 </template>
 
@@ -15,6 +36,11 @@ export default {
       type: Object,
       default: () => {},
     },
+  },
+  data() {
+    return {
+      newComment: '',
+    }
   },
 }
 </script>
