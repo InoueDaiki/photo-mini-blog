111a285b651da50600ffe3b5f1d6a73541d96f5d
Inoue Daiki <daiki.inoue@automagi.jp>
2020-11-01 16:32:40 +0900

    docs: remove new page

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,14 +5,6 @@
         <nuxt-link to="/">
           <b-navbar-brand>Photo mini blog</b-navbar-brand>
         </nuxt-link>
-
-        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-        <b-collapse id="nav-collapse" is-nav>
-          <b-navbar-nav>
-            <b-nav-item to="/new">投稿する</b-nav-item>
-          </b-navbar-nav>
-        </b-collapse>
       </b-container>
     </b-navbar>
     <b-container class="my-3">
