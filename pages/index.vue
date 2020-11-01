9307bfa013b51293365891a535d3b19ece94e0f3
Inoue Daiki <daiki.inoue@automagi.jp>
2020-11-01 16:54:08 +0900

    docs: init index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,7 @@
 <template>
-  <b-row align-h="center">
-    <b-col cols="12" sm="6">
-      {{ posts }}
-    </b-col>
-  </b-row>
+  <div>index.vue</div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      posts: [
-        {
-          id: 'UUID will be here',
-          username: 'Alice',
-          s3key: 'UUID will be here',
-          imageUrl: 'https://picsum.photos/400/300',
-          comments: {
-            items: [
-              {
-                createdAt: new Date().toLocaleString(),
-                username: 'Alice',
-                content: 'First comment',
-              },
-              {
-                createdAt: new Date().toLocaleString(),
-                username: 'Bob',
-                content: 'Seccond comment',
-              },
-            ],
-          },
-        },
-      ],
-    }
-  },
-}
+export default {}
 </script>
