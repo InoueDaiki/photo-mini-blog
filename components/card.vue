d072a6f76b26a66712fe76912ef8d9f92aaeae41
Inoue Daiki <daiki.inoue@automagi.jp>
2020-11-01 16:50:28 +0900

    docs: remove comment form

--- components/card.vue
+++ components/card.vue
@@ -1,31 +1,10 @@
 <template>
-  <b-card :img-src="post.imageUrl" footer-bg-variant="white">
+  <b-card :img-src="post.imageUrl">
     <div v-for="comment in post.comments.items" :key="comment.id">
       <strong>{{ comment.username }}</strong>
       {{ comment.content }} <br />
       <small>{{ comment.createdAt }}</small>
     </div>
-
-    <template #footer>
-      <b-form>
-        <b-input-group>
-          <b-form-input
-            v-model="newComment"
-            placeholder="コメントを追加..."
-            class="border-0 shadow-none"
-          ></b-form-input>
-          <b-input-group-append>
-            <b-button
-              type="submit"
-              variant="link"
-              class="text-decoration-none shadow-none"
-            >
-              投稿する
-            </b-button>
-          </b-input-group-append>
-        </b-input-group>
-      </b-form>
-    </template>
   </b-card>
 </template>
 
@@ -36,11 +15,6 @@ export default {
       type: Object,
       default: () => {},
     },
-  },
-  data() {
-    return {
-      newComment: '',
-    }
   },
 }
 </script>
