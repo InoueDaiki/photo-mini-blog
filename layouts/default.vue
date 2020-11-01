4b1d32c1ec4a5107dbc10804c44b508f7c87a2be
Inoue Daiki <daiki.inoue@automagi.jp>
2020-11-01 16:57:01 +0900

    docs: init default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,5 @@
 <template>
-  <div>
-    <b-navbar toggleable="sm" variant="light" sticky class="border-bottom">
-      <b-container>
-        <nuxt-link to="/">
-          <b-navbar-brand>Photo mini blog</b-navbar-brand>
-        </nuxt-link>
-      </b-container>
-    </b-navbar>
-    <b-container class="my-3">
-      <Nuxt />
-    </b-container>
-  </div>
+  <div></div>
 </template>
 
 <script>
